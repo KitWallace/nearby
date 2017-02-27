1de10c32f72e3d9f664c00068e00f0a0896c02a0
Chris Wallace <kit.wallace@gmail.com>
2017-02-27 13:10:44 +0000

    moved load range
    load range in html page - html generation changed

--- js/monument-config.js
+++ js/monument-config.js
@@ -1,31 +1,30 @@
 var root = "";
 var item_name = "Monument";
 var items_in_range_url = "xquery/monuments-in-range.xq";
-var edit_url = "xquery/edit_monument.xq";
 var here_icon = "assets/redbullseye.png";
 var nearest_icon = "assets/camera-highlight.png";
 var item_icon = "assets/camera.png";
 
-
-var zoom=18;
-var load_range=3000;
+var zoom=16;
 
 function item_to_html(item) {
-   var html =  "<div>"+item.id+" <em>"+item.title+"</em>"+ "             &#160;<br/>";
-   html+=  item.description;
+   var html =  '<div>';
+   html += '<h2>'+item.title+'</h2>'
+   html += '<h3>'+item.period+'</h3>';
+   html +=  '<div>'+item.description +'</div>';
    html += "</div>";
    return html;
 };
 
-function item_to_summary(item,dist,dir,compass) {
+function item_to_summary(item) {
    var html =  "<div>";
-   html+= '<div>'+item.id+' <b>'+item.title+'</b>'+ '</div>';
-   html+= '<div>'+' [' + dist+ "m at " + dir + '° ' + compass  +  ']' +'</div>';   html += "</div>";
+   html+= '<h2>'+item.title+'</h2>';
+   html += "</div>";
    return html;
 };
 
 function item_to_description(item) {
-    return '<div><h2>'+item.title+'</h2>'+item.description + '</div>';
+    return '<div><h2>'+item.title+'</h2><h3>Period:'+ item.period+'</h3>'+ item.description + '</div>';
 };
 
 var debug = false;
